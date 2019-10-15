dafb25b58df753f949528109d4ba2cff056fcfe6
Esteban Monge <emonge@gbm.net>
2019-10-15 15:33:05 -0600

    add performance data in minutes to check uptime by ssh plugin

--- check_uptime_by_ssh.sh
+++ check_uptime_by_ssh.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 ##########################################################
 # Plugin to check uptime via SSH to avoid use of agents
-# Works on AIX, Debian and RHEL alike GNU/Linux 
+# Works on AIX, Debian and RHEL alike GNU/Linux
 # Esteban Monge: estebanmonge@riseup.net
 # https://github.com/EstebanMonge/nagios-plugins
 ############################################################
-
 
 help(){
 PROGNAME=$(basename $0)
@@ -22,23 +21,41 @@ then
         exit 3
 fi
 
-output=$(ssh $host 'uptime' 2> /dev/null | awk '{print $3,$4}' )
+performance=$(ssh $host 'uptime' 2> /dev/null )
+output=$(echo $performance | awk '{print $3,$4}')
+
+case $performance in
+        *min*)
+                echo $performance | awk '{print $3}'
+                ;;
+        *day*)
+                time_today=$(echo $performance | awk '{print $1}')
+                total_seconds=$(($(date -u -d "$time_today" +"%s")-$(date -u -d "12:00AM" +"%s")))
+                minutes_today=$((total_seconds/60))
+                total_minutes=$(echo $performance | awk '{print $3*24*60}')
+                performance=$((total_minutes+minutes_today))
+                ;;
+        *)
+                time_uptime=$(echo $performance | awk '{print $3}')
+                performance=$(echo $time_uptime | awk -F: '{print $1*60+$2}')
+                ;;
+esac
 
 case $output in
-	0-1) echo "CRITICAL - Uptime: ${output//,}"
-	   exit 2
-	   ;;
-	*min*) echo "CRITICAL - Uptime: ${output//,}"
-	   exit 2
-	   ;;
+        0-1) echo "CRITICAL - Uptime: ${output//,} | uptime_minutes=$performance"
+           exit 2
+           ;;
+        *min*) echo "CRITICAL - Uptime: ${output//,} | uptime_minutes=$performance"
+           exit 2
+           ;;
         "") echo "CRITICAL - I can't get uptime. Agüevado"
-	    exit 2
-	    ;;
-	*) echo "OK - Uptime: ${output//,}"
+            exit 2
+            ;;
+        *) echo "OK - Uptime: ${output//,} | uptime_minutes=$performance"
            exit 0
-	   ;;
+           ;;
 esac
-        
+
 
 if [[ $output != "0" && $output != "1" && $output != "" ]]
 then
