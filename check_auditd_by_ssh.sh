87eb2d83d363d1196bc2e89c7417ea5e03b373c8
Esteban Monge <emonge@gbm.net>
2019-10-25 10:39:08 -0600

    pretty audit rules

--- check_auditd_by_ssh.sh
+++ check_auditd_by_ssh.sh
@@ -22,13 +22,13 @@ then
         exit 3
 fi
 
-output=$(ssh $host "sudo /usr/sbin/ausearch --input-logs -ts today -m $message_type" 2> /dev/null )
+output=$(ssh $host "/bin/sudo /usr/sbin/ausearch --input-logs -ts today -m $message_type --raw| /usr/sbin/aureport -f --summary | grep '^[0-9]'" 2> /dev/null )
 
 if [[ -z $output ]]
 then
         echo "OK - No $message_type logs found"
         exit 0
 else
-        echo -e "CRITICAL - I found $message_type logs:\n $output"
+        echo -e "CRITICAL - I found $message_type logs:\n$output"
         exit 2
 fi
