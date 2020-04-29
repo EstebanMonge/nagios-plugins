a8622dca1658b6ae15c3ca386f92f3b2992fe522
Esteban Monge <estebanmonge@riseup.net>
2020-04-28 20:23:29 -0600

    fix for orders with invalido  state

--- check_odoo_pos_fe.py
+++ check_odoo_pos_fe.py
@@ -29,7 +29,7 @@ class Odoo():
 
 # Funcion para obtener las ordenes del pos segun estado
     def posorderCheck(self, stateOrder):
-        odoo_filter = [[("state_tributacion", '!=', 'aceptado'),("state_tributacion", '!=', 'rechazado'),("state_tributacion", '!=', 'no_aplica'),("state_tributacion", '!=', 'error'),("state_tributacion", '!=', 'rejected'),("state_tributacion", '!=', 'no_encontrado'),("state_tributacion", '!=', 'firma_invalida'),("state_tributacion", '!=', 'procesando')]]
+        odoo_filter = [[("state_tributacion", '!=', 'aceptado'),("state_tributacion", '!=', 'rechazado'),("state_tributacion", '!=', 'no_aplica'),("state_tributacion", '!=', 'error'),("state_tributacion", '!=', 'rejected'),("state_tributacion", '!=', 'no_encontrado'),("state_tributacion", '!=', 'firma_invalida'),("state_tributacion", '!=', 'procesando'),("state_tributacion", '!=', 'invalido')]]
         posorder_id = self.ODOO_OBJECT.execute_kw(
             self.DATA
 	    , self.UID
